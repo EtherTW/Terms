b3178edf9c3fb34838a3043ebacac8b42069bbe0
Anton Cheng <antonassocareer@gmail.com>
2023-06-20 13:22:37 +0200

    build: css template (#10)

---
+++ src/html_builder.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::Write;
+
+use crate::terms::Terms;
+
+fn generate_terms_table(terms: &Terms) -> String {
+    let mut table = String::new();
+    for term in &terms.terms {
+        table.push_str("<tr>");
+        table.push_str(&format!("<td>{}</td>", term.term));
+        table.push_str(&format!("<td>{}</td>", term.translation));
+        table.push_str("</tr>\n");
+    }
+    table
+}
+
+pub fn build_static_page(terms: &Terms, output_path: &str) -> Result<(), Box<dyn Error>> {
+    let mut template = fs::read_to_string("build/template.html")?;
+    let terms_table = generate_terms_table(terms);
+    template = template.replace("<!-- TERMS_TABLE_PLACEHOLDER -->", &terms_table);
+    let mut output_file = File::create(output_path)?;
+    write!(output_file, "{}", template)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,11 @@
-use serde::{Deserialize, Serialize};
 use std::env;
 use std::error::Error;
-use std::fs::{self, read_to_string, write};
-use toml::{from_str, to_string};
 
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-struct Term {
-    term: String,
-    translation: String,
-}
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-struct Terms {
-    terms: Vec<Term>,
-}
+mod html_builder;
+mod terms;
 
-impl Terms {
-    fn from_file(path: &str) -> Result<Self, Box<dyn Error>> {
-        let contents = read_to_string(path)?;
-        let terms: Terms = from_str(&contents)?;
-        Ok(terms)
-    }
-
-    /// Sort the terms alphabetically by term
-    fn sort_terms(&mut self) -> Self {
-        self.terms
-            .sort_by(|a, b| a.term.to_lowercase().cmp(&b.term.to_lowercase()));
-        self.clone()
-    }
-
-    fn to_file(&self, path: &str) -> Result<(), Box<dyn Error>> {
-        let contents = to_string(self)?;
-        write(path, contents)?;
-        Ok(())
-    }
-}
-
-fn build_static_page(terms: Terms, output_path: &str) {
-    // Generate the HTML output
-    let mut html = String::new();
-    html.push_str("<html>\n");
-    html.push_str("<head>\n");
-    html.push_str(r#"<meta charset="utf-8">"#);
-    html.push_str("<title>Translated Terms</title>\n");
-    html.push_str("</head>\n");
-    html.push_str("<body>\n");
-    html.push_str("<h1>Translated Terms</h1>\n");
-    html.push_str("<table>\n");
-    html.push_str("<tr><th>English Term</th><th>中文</th></tr>\n");
-
-    // Iterate over each term in the terms table and append to the HTML output
-    for term in terms.terms {
-        html.push_str("<tr>");
-        html.push_str(&format!("<td>{}</td>", term.term));
-        html.push_str(&format!("<td>{}</td>", term.translation));
-        html.push_str("</tr>\n");
-    }
-
-    html.push_str("</table>\n");
-    html.push_str("</body>\n");
-    html.push_str("</html>");
-
-    // Write the HTML output to a file
-    fs::write(output_path, html).expect("Failed to write terms.html");
-}
+use html_builder::build_static_page;
+use terms::Terms;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -73,14 +16,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     if check_only {
         // check #1: make sure terms are all sorted
-        let expected = terms.clone().sort_terms();
+        let expected = terms.clone();
+        terms.sort_terms();
         if terms != expected {
             panic!("terms.toml is not sorted");
         }
         // more checks to be added
     } else {
-        terms.sort_terms().to_file(path)?;
-        build_static_page(terms, "build/index.html");
+        terms.sort_terms();
+        terms.to_file(path)?;
+        build_static_page(&terms, "build/index.html")?;
     }
 
     Ok(())

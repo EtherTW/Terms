efc64336255ccc7cb5a61af3d54734f59c521bfe
ryanycw.eth <63644531+ryanycw@users.noreply.github.com>
2025-06-11 17:50:12 +0800

    chore: add new terms (#48)

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,36 @@ mod html_builder;
 mod terms;
 
 use html_builder::build_static_page;
-use terms::{Tags, Terms};
+use terms::{Tags, Terms, ToFile};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let check_only = args.len() > 1 && args[1] == "--check";
 
-    let path = "terms.toml";
-    let mut terms = Terms::load_terms(path)?;
+    let terms_path = "terms.toml";
+    let tags_path = "tags.toml";
+    let mut terms = Terms::load_terms(terms_path)?;
+    let mut tags = Tags::load_tags(tags_path)?;
 
     if check_only {
         // check #1: make sure terms are all sorted
-        let expected = terms.clone();
+        let expected_terms = terms.clone();
         terms.sort_terms();
-        if terms != expected {
-            panic!("terms.toml is not sorted");
-        }
-        // check #2: make sure all tags are valid
-        let tags = Tags::load_tags("tags.toml")?;
+        assert!(terms == expected_terms, "terms.toml is not sorted");
+
+        // check #2: make sure all tags are all sorted
+        let expected_tags = tags.clone();
+        tags.sort_tags();
+        assert!(tags == expected_tags, "tags.toml is not sorted");
+
+        // check #3: make sure all tags are valid
         terms.check_all_tags(tags)?;
     } else {
         terms.sort_terms();
-        terms.to_file(path)?;
+        terms.to_file(terms_path)?;
+        tags.sort_tags();
+        tags.to_file(tags_path)?;
+
         build_static_page(&terms, "build/index.html")?;
     }
 
--- src/terms.rs
+++ src/terms.rs
@@ -3,6 +3,16 @@ use std::error::Error;
 use std::fs::{read_to_string, write};
 use toml::{from_str, to_string};
 
+pub trait ToFile: Serialize {
+    fn to_file(&self, path: &str) -> Result<(), Box<dyn Error>> {
+        let contents = to_string(self)?;
+        write(path, contents)?;
+        Ok(())
+    }
+}
+
+impl<T: Serialize> ToFile for T {}
+
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct Term {
     pub term: String,
@@ -17,13 +27,13 @@ pub struct Terms {
     pub terms: Vec<Term>,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct Tag {
     pub id: String,
     pub description: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 pub struct Tags {
     pub tags: Vec<Tag>,
 }
@@ -52,12 +62,6 @@ impl Terms {
         self.terms
             .sort_by(|a, b| a.term.to_lowercase().cmp(&b.term.to_lowercase()));
     }
-
-    pub fn to_file(&self, path: &str) -> Result<(), Box<dyn Error>> {
-        let contents = to_string(self)?;
-        write(path, contents)?;
-        Ok(())
-    }
 }
 
 impl Tags {
@@ -74,5 +78,10 @@ impl Tags {
             }
         }
         false
+    }
+
+    pub fn sort_tags(&mut self) {
+        self.tags
+            .sort_by(|a, b| a.id.to_lowercase().cmp(&b.id.to_lowercase()));
     }
 }
